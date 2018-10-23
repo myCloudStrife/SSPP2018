735b0c836856bfe6d48769b4c0b34bcd1879f638
Alex <Alextmail@yandex.ru>
2018-10-23 21:23:12 +0300

    task2 upgrade

--- task2/main.cpp
+++ task2/main.cpp
@@ -5,15 +5,30 @@
 #include <chrono>
 #include <papi.h>
 
-constexpr int EFFECTIVE_CACHE_SIZE = 45;
-constexpr int NUM_EVENTS = 2;
-const char *modes[] = { "time", "L1_cache_misses", "L2_cache_misses", "Total_cycles" };
-//int events[NUM_EVENTS] = { PAPI_L1_TCM, PAPI_L2_TCM };//, PAPI_TOT_CYC };
+constexpr int EFFECTIVE_CACHE_SIZE = 72;
+const char *events_name[] = { "Time", "L1_cache_misses", "L2_cache_misses", "Total_cycles",
+        "Flop", "TLB_misses" };
 
 int repeat = 1;
-double work_time[3] = { 0, 0, 0 };
 
 using namespace std;
+
+struct Papi_data {
+    long long time = 0;
+    long_long l1_misses = 0;
+    long_long l2_misses = 0;
+    long_long total_cycles = 0;
+    long_long flop = 0;
+    long_long tlb_misses = 0;
+    void operator+=(const Papi_data & x) {
+        time += x.time;
+        l1_misses += x.l1_misses;
+        l2_misses += x.l2_misses;
+        total_cycles += x.total_cycles;
+        flop += x.flop;
+        tlb_misses += x.tlb_misses;
+    }
+};
 
 struct Matrix {
     float *data = nullptr;
@@ -71,115 +86,105 @@ void writeMatrix(const char *filename, Matrix & matrix) {
     out.close();
 }
 
-Matrix mulMatrix(Matrix & A, Matrix & B, int mode, long_long *values) {
+Matrix mulMatrix(Matrix & A, Matrix & B, int mode, Papi_data & value) {
     Matrix C = Matrix(A.rows, B.columns);
-    int event[1];
-    long_long tmpvalue;
-    switch (mode) {
-    case 1:
-        event[0] = PAPI_L1_TCM;
-        break;
-    case 2:
-        event[0] = PAPI_L2_TCM;
-        break;
-    case 3:
-        event[0] = PAPI_TOT_CYC;
-        break;
-    }
-    chrono::steady_clock::time_point time1, time2;
-    if (mode) {
-        if (PAPI_start_counters(event, 1) != PAPI_OK) {
-            fprintf(stderr, "PAPI_start_counters failed\n");
-            exit(1);
+    Papi_data tmpvalue;
+    long long time1, time2;
+    for (int i = 0; i < 2; ++i) {
+        if (!i) {
+            int events[] = { PAPI_L1_TCM, PAPI_L2_TCM, PAPI_TOT_CYC };
+            if (PAPI_start_counters(events, 3) != PAPI_OK) {
+                fprintf(stderr, "PAPI_start_counters failed\n");
+                exit(1);
+            }
+        } else {
+            int events[] = { PAPI_FP_OPS, PAPI_TLB_DM };
+            if (PAPI_start_counters(events, 2) != PAPI_OK) {
+                fprintf(stderr, "PAPI_start_counters failed\n");
+                exit(1);
+            }
         }
-    } else {
-        time1 = chrono::steady_clock::now();
-    }
-    for (uint64_t i = 0; i < A.rows; i += 32) {
-        for (uint64_t j = 0; j < B.columns; j += 32) {
-            for (uint64_t k = 0; k < A.columns; k += 32) {
-                for (uint64_t i2 = i; i2 < min(i + 32, A.rows); ++i2) {
-                    for (uint64_t j2 = j; j2 < min(j + 32, B.columns); ++j2) {
-                        for (uint64_t k2 = k; k2 < min(k + 32, A.columns); ++k2) {
-                            C[i2][j2] += A[i2][k2] * B[k2][j2];
+        switch (mode) {
+        case 0:
+            time1 = PAPI_get_real_usec();
+            for (uint64_t i = 0; i < A.rows; i += 32) {
+                for (uint64_t j = 0; j < B.columns; j += 32) {
+                    for (uint64_t k = 0; k < A.columns; k += 32) {
+                        for (uint64_t i2 = i; i2 < min(i + 32, A.rows); ++i2) {
+                            for (uint64_t j2 = j; j2 < min(j + 32, B.columns); ++j2) {
+                                for (uint64_t k2 = k; k2 < min(k + 32, A.columns); ++k2) {
+                                    C[i2][j2] += A[i2][k2] * B[k2][j2];
+                                }
+                            }
                         }
                     }
                 }
             }
-        }
-    }
-    if (mode) {
-        if (PAPI_read_counters(&tmpvalue, 1) != PAPI_OK) {
-            fprintf(stderr, "PAPI_read_counters failed\n");
-            exit(1);
-        }
-        values[0] += tmpvalue;
-        /*if (PAPI_start_counters(event, 1) != PAPI_OK) {
-            fprintf(stderr, "PAPI_start_counters failed\n");
-            exit(1);
-        }*/
-    } else {
-        time2 = chrono::steady_clock::now();
-        work_time[0] += chrono::duration_cast<chrono::duration<double>>(time2 - time1).count();
-        time1 = chrono::steady_clock::now();
-    }
-    for (uint64_t i = 0; i < A.rows; i += 32) {
-        for (uint64_t k = 0; k < A.columns; k += 32) {
-            for (uint64_t j = 0; j < B.columns; j += 32) {
-                for (uint64_t i2 = i; i2 < min(i + 32, A.rows); ++i2) {
-                    for (uint64_t k2 = k; k2 < min(k + 32, A.columns); ++k2) {
-                        for (uint64_t j2 = j; j2 < min(j + 32, B.columns); ++j2) {
-                            C[i2][j2] += A[i2][k2] * B[k2][j2];
+            time2 = PAPI_get_real_usec();
+            break;
+        case 1:
+            time1 = PAPI_get_real_usec();
+            for (uint64_t i = 0; i < A.rows; i += 32) {
+                for (uint64_t k = 0; k < A.columns; k += 32) {
+                    for (uint64_t j = 0; j < B.columns; j += 32) {
+                        for (uint64_t i2 = i; i2 < min(i + 32, A.rows); ++i2) {
+                            for (uint64_t k2 = k; k2 < min(k + 32, A.columns); ++k2) {
+                                for (uint64_t j2 = j; j2 < min(j + 32, B.columns); ++j2) {
+                                    C[i2][j2] += A[i2][k2] * B[k2][j2];
+                                }
+                            }
                         }
                     }
                 }
             }
-        }
-    }
-    if (mode) {
-        if (PAPI_read_counters(&tmpvalue, 1) != PAPI_OK) {
-            fprintf(stderr, "PAPI_read_counters failed\n");
-            exit(1);
-        }
-        values[1] += tmpvalue;
-        /*if (PAPI_start_counters(event, 1) != PAPI_OK) {
-            fprintf(stderr, "PAPI_start_counters failed\n");
-            exit(1);
-        }*/
-    } else {
-        time2 = chrono::steady_clock::now();
-        work_time[1] += chrono::duration_cast<chrono::duration<double>>(time2 - time1).count();
-        time1 = chrono::steady_clock::now();
-    }
-    for (uint64_t i = 0; i < A.rows; i += EFFECTIVE_CACHE_SIZE) {
-        for (uint64_t k = 0; k < A.columns; k += EFFECTIVE_CACHE_SIZE) {
-            for (uint64_t j = 0; j < B.columns; j += EFFECTIVE_CACHE_SIZE) {
-                for (uint64_t i2 = i; i2 < min(i + EFFECTIVE_CACHE_SIZE, A.rows); ++i2) {
-                    for (uint64_t k2 = k; k2 < min(k + EFFECTIVE_CACHE_SIZE, A.columns); ++k2) {
-                        for (uint64_t j2 = j; j2 < min(j + EFFECTIVE_CACHE_SIZE, B.columns); ++j2) {
-                            C[i2][j2] += A[i2][k2] * B[k2][j2];
+            time2 = PAPI_get_real_usec();
+            break;
+        case 2:
+            time1 = PAPI_get_real_usec();
+            for (uint64_t i = 0; i < A.rows; i += EFFECTIVE_CACHE_SIZE) {
+                for (uint64_t k = 0; k < A.columns; k += EFFECTIVE_CACHE_SIZE) {
+                    for (uint64_t j = 0; j < B.columns; j += EFFECTIVE_CACHE_SIZE) {
+                        for (uint64_t i2 = i; i2 < min(i + EFFECTIVE_CACHE_SIZE, A.rows); ++i2) {
+                            for (uint64_t k2 = k; k2 < min(k + EFFECTIVE_CACHE_SIZE, A.columns); ++k2) {
+                                for (uint64_t j2 = j; j2 < min(j + EFFECTIVE_CACHE_SIZE, B.columns); ++j2) {
+                                    C[i2][j2] += A[i2][k2] * B[k2][j2];
+                                }
+                            }
                         }
                     }
                 }
             }
+            time2 = PAPI_get_real_usec();
+            break;
+        }
+        if (!i) {
+            if (PAPI_stop_counters(&tmpvalue.l1_misses, 3) != PAPI_OK) {
+                fprintf(stderr, "PAPI_stop_counters failed\n");
+                exit(1);
+            }
+            if (time2 < time1) {
+                fprintf(stderr, "time error\n");
+                exit(1);
+            }
+            tmpvalue.time = time2 - time1;
+        } else {
+            if (PAPI_stop_counters(&tmpvalue.flop, 2) != PAPI_OK) {
+                fprintf(stderr, "PAPI_stop_counters failed\n");
+                exit(1);
+            }
+            if (time2 < time1) {
+                fprintf(stderr, "time error\n");
+                exit(1);
+            }
+            tmpvalue.time = (time2 - time1 + tmpvalue.time) / 2;
         }
     }
-    if (mode) {
-        if (PAPI_stop_counters(&tmpvalue, 1) != PAPI_OK) {
-            fprintf(stderr, "PAPI_stop_counters failed\n");
-            exit(1);
-        }
-        values[2] += tmpvalue;
-        //if (PAPI_)
-    } else {
-        time2 = chrono::steady_clock::now();
-        work_time[2] += chrono::duration_cast<chrono::duration<double>>(time2 - time1).count();
-    }
+    value += tmpvalue;
     return C;
 }
 
 void usage(int argc, char **argv) {
-    printf("Usage: %s <matrix1> <matrix2> <output_filename> <mode> [repeat_count]>\n", argv[0]);
+    printf("Usage: %s <matrix1> <matrix2> <output_file> <index_mode> [repeat_count]>\n", argv[0]);
     exit(1);
 }
 
@@ -187,28 +192,30 @@ int main(int argc, char **argv) {
     if (argc != 5 && argc != 6) {
         usage(argc, argv);
     }
-    int mode;
+    int index_mode;
     if (argc == 6) {
         sscanf(argv[5], "%d", &repeat);
     }
-    sscanf(argv[4], "%d", &mode);
+    sscanf(argv[4], "%d", &index_mode);
     PAPI_library_init(PAPI_VER_CURRENT);
-    long_long values[3] = { 0, 0, 0 };
+    Papi_data value;
     Matrix A = Matrix(argv[1]);
     Matrix B = Matrix(argv[2]);
-    Matrix C = mulMatrix(A, B, mode, values);
-    printf("# mode %15s\n", modes[mode]);
+    if (A.columns != B.rows) {
+        printf("Incorrect input matrixes\n");
+        exit(1);
+    }
+    Matrix C = mulMatrix(A, B, index_mode, value);
+    printf("# size");
+    for (int i = 0; i < 6; ++i) {
+        printf(" %15s", events_name[i]);
+    }
+    printf("\n");
     for (int i = 1; i < repeat; ++i) {
-        C = mulMatrix(A, B, mode, values);
+        C = mulMatrix(A, B, index_mode, value);
     }
     writeMatrix(argv[3], C);
-    if (mode) {
-        cout << setw(6) << 0 << ' ' << setw(15) << values[0] / repeat << endl;
-        cout << setw(6) << 1 << ' ' << setw(15) << values[1] / repeat << endl;
-        cout << setw(6) << 2 << ' ' << setw(15) << values[2] / repeat << endl;
-    } else {
-        cout << setw(6) << 0 << ' ' << setw(15) << work_time[0] / repeat << endl;
-        cout << setw(6) << 1 << ' ' << setw(15) << work_time[1] / repeat << endl;
-        cout << setw(6) << 2 << ' ' << setw(15) << work_time[2] / repeat << endl;
-    }
+    printf("%6lu %15lld %15lld %15lld %15lld %15lld %15lld\n", A.rows, value.time / repeat,
+            value.l1_misses / repeat, value.l2_misses / repeat, value.total_cycles / repeat,
+            value.flop / repeat, value.tlb_misses / repeat);
 }
--- task2/plot.gnu
+++ task2/plot.gnu
@@ -1,5 +1,4 @@
 set terminal svg size 600,500
 set title "Task2"
-set xlabel 'Mode'
-set xtics ("ijk,32x32" 0, "ikj,32x32" 1, "ikj,45x45" 2)
+set xlabel 'Matrix size'
 set object 1 rectangle from screen 0,0 to screen 1,1 fillcolor rgb "white" behind
