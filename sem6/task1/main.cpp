0de39fcae9d954dd5a8d0b5ee95da9ed073e246a
Alex <Alextmail@yandex.ru>
2019-02-14 09:17:32 +0300

    add print_vec

--- sem6/task1/main.cpp
+++ sem6/task1/main.cpp
@@ -26,7 +26,8 @@ vector<complex<double>> rand_vec_norm(unsigned long long n) {
     return vec;
 }
 
-vector<complex<double>> transform(vector<complex<double>> a, int k, vector<complex<double>> u) {
+vector<complex<double>> transform(const vector<complex<double>> & a, int k,
+        const vector<complex<double>> & u) {
     vector<complex<double>> b(a.size());
     unsigned long long bit = 1ull << k;
 #pragma omp parallel for
@@ -34,6 +35,12 @@ vector<complex<double>> transform(vector<complex<double>> a, int k, vector<compl
         b[i] = a[i & ~bit] * u[(i & bit) >> (k - 1)] + a[i | bit] * u[((i & bit) >> (k - 1)) + 1];
     }
     return b;
+}
+
+void print_vec(const vector<complex<double>> & a) {
+    for (unsigned long long i = 0; i < a.size(); ++i) {
+        printf("%f %f\n", a[i].real(), a[i].imag());
+    }
 }
 
 int main(int argc, char **argv) {
@@ -50,6 +57,7 @@ int main(int argc, char **argv) {
 
     double start_time = omp_get_wtime();
     vector<complex<double>> a = rand_vec_norm(1ull << n);
+    print_vec(a);
     transform(a, k, u);
     printf("%.10f\n", omp_get_wtime() - start_time);
 }
